6de5107b6b56a16f4392e1e22e42f3f95f9705fb
madaracher <madaracher@gmail.com>
2026-03-30 21:49:29 +0200

    add name in the Main files

--- src/test/Main.java
+++ src/test/Main.java
@@ -15,6 +15,8 @@ import primitives.*;
  * The program prints error messages only if a test fails.
  * If no output appears, all tests succeeded.
  * @author Dan Zilberstein
+ * @author Acher Madar
+ * @author Mattias Bensimon
  */
 @SuppressWarnings("java:S109")
 public final class Main {
